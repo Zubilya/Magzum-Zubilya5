15ff29c85c5a8cebabbba61d5a0c7c3d707aec32
Zubilya <96620482+Zubilya@users.noreply.github.com>
2022-01-31 20:09:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//task1
+var name = prompt("Please, enter your name");
+document.write(name);
+
+var year = prompt("Please enter year");
+var week = prompt("Please enter weekday");
+var date1 = prompt("Please enter date");
+var month = prompt("Please enter month name");
+var time = new Date();
+var today = new Date();
+
+//task2
+document.write("Year is: " + year + "<br>");
+document.write("Today is " + week + "<br>");
+document.write("Date: " + date1 + "<br>");
+document.write("Month: " + month + "<br>");
+document.write("Current time is " + new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' }));
+
+//task3
 var date1, date2;
 var today = prompt("Enter the today date: 2022-1-31")
 
